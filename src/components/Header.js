14cdf2226ddbdde5b7c931a9168fdc77a608b3b8
Nelson Jr <juniordell123@gmail.com>
2020-08-19 09:50:43 -0300

    add transition options and hover animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,6 +19,7 @@ const Header = () => {
       <motion.div className="title"
         initial={{ y: -250 }}
         animate={{ y: -10 }}
+        transition={{ delay: 1.5, type: 'spring', stiffness: 120 }}
       >
         <h1>Pizza Joint</h1>
       </motion.div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,16 @@ const Home = () => {
     <motion.div className="home container"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
+      transition={{ delay: 1.5, duration: 1.5 }}
     >
       <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
         <motion.button
-          animate={{  }}
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgb(255, 255, 255)",
+            boxShadow: "0px 0px 8px rgb(255, 255, 255)"
+          }}
         >
           Create Your Pizza
         </motion.button>
